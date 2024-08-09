bf61d87dede6ccc548f6e021ac3a664d0b326fee
khlea-ishere01 <khleajazz@gmail.com>
2024-08-09 09:56:52 +0800

    Update listbox.js

--- scripts/cmds/listbox.js
+++ scripts/cmds/listbox.js
@@ -1,80 +1,41 @@
-module.exports.config = {
-    name: "kiss",
-    version: "2.0.0",
-    hasPermssion: 0,
-    credits: "ZiaRein",
-    description: "kiss someone",
-    commandCategory: "Love",
-    usages: `Please tag 1 person\n\nHow to use?\n${global.config.PREFIX}kiss <@tag>\n\nExample:\n${global.config.PREFIX}kiss @name\n`,
-    cooldowns: 5,
-    dependencies: {
-        "axios": "",
-        "fs-extra": "",
-        "path": "",
-        "jimp": ""
-    }
+module.exports = {
+        config: {
+                name: "listbox",
+                aliases: [],
+                author: "kshitiz",
+                version: "2.0",
+                cooldowns: 5,
+                role: 2,
+                shortDescription: {
+                        en: "List all group chats the bot is in."
+                },
+                longDescription: {
+                        en: "Use this command to list all group chats the bot is currently in."
+                },
+                category: "owner",
+                guide: {
+                        en: "{p}{n} "
+                }
+        },
+        onStart: async function ({ api, event }) {
+                try {
+                        const groupList = await api.getThreadList(100, null, ['INBOX']);
+
+
+                        const filteredList = groupList.filter(group => group.threadName !== null);
+
+                        if (filteredList.length === 0) {
+
+                                await api.sendMessage('No group chats found.', event.threadID);
+                        } else {
+                                const formattedList = filteredList.map((group, index) =>
+                                        `│${index + 1}. ${group.threadName}\n│𝐓𝐈𝐃: ${group.threadID}`
+                                );
+                                const message = `╭─╮\n│𝐋𝐢𝐬𝐭 𝐨𝐟 𝐠𝐫𝐨𝐮𝐩 𝐜𝐡𝐚𝐭𝐬:\n${formattedList.map(line => `${line}`).join("\n")}\n╰───────────ꔪ`;
+                                await api.sendMessage(message, event.threadID, event.messageID);
+                        }
+                } catch (error) {
+                        console.error("Error listing group chats", error);
+                }
+        },
 };
-
-module.exports.onLoad = async() => {
-    const { resolve } = global.nodemodule["path"];
-    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-    const { downloadFile } = global.utils;
-    const dirMaterial = __dirname + `/cache/`;
-    const path = resolve(__dirname, 'cache', 'hon.png');
-    if (!existsSync(dirMaterial + "")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.imgur.com/BtSlsSS.jpg", path);
-
-}
-
-async function makeImage({ one, two }) {
-    const fs = global.nodemodule["fs-extra"];
-    const path = global.nodemodule["path"];
-    const axios = global.nodemodule["axios"]; 
-    const jimp = global.nodemodule["jimp"];
-    const __root = path.resolve(__dirname, "cache");
-
-    let hon_img = await jimp.read(__root + "/hon.png");
-    let pathImg = __root + `/hon_${one}_${two}.png`;
-    let avatarOne = __root + `/avt_${one}.png`;
-    let avatarTwo = __root + `/avt_${two}.png`;
-
-    let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
-    fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
-
-    let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
-    fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
-
-    let circleOne = await jimp.read(await circle(avatarOne));
-    let circleTwo = await jimp.read(await circle(avatarTwo));
-    hon_img.resize(700, 440).composite(circleOne.resize(200, 200), 390, 23).composite(circleTwo.resize(180, 180), 140, 80);
-
-    let raw = await hon_img.getBufferAsync("image/png");
-
-    fs.writeFileSync(pathImg, raw);
-    fs.unlinkSync(avatarOne);
-    fs.unlinkSync(avatarTwo);
-
-    return pathImg;
-}
-async function circle(image) {
-    const jimp = require("jimp");
-    image = await jimp.read(image);
-    image.circle();
-    return await image.getBufferAsync("image/png");
-}
-
-module.exports.run = async function ({ event, api, args, Currencies }) { 
-    const fs = global.nodemodule["fs-extra"];
-    const ae = ["💚 congrats❤","💙 congrats💜"];
-    const hc = Math.floor(Math.random() * 101) + 101;
-    const rd = Math.floor(Math.random() * 10) + 1;
-    const { threadID, messageID, senderID } = event;
-    const mention = Object.keys(event.mentions);
-    var one = senderID, two = mention[0];
-  await Currencies.increaseMoney(event.senderID, parseInt(hc*rd));
-
-  if (!two) return api.sendMessage(`Please tag 1 person\n\nHow to use?\n${global.config.PREFIX}kiss <@tag>\n\nExample:\n${global.config.PREFIX}kiss @name\n\nCreated by: CaNDY (LaFhanGa chokra)`, threadID, messageID);
-  else {
-        return makeImage({ one, two }).then(path => api.sendMessage({ body: `${ae[Math.floor(Math.random() * ae.length)]}\nYour sympathy after being stolen is ${hc} %\n + ${((hc)*rd)} $`, attachment: fs.createReadStream(path)}, threadID, () => fs.unlinkSync(path), messageID));
-  }
-  }
