898b826281f6a0ddb020a5354f0e2537897ba91d
khlea-ishere01 <khleajazz@gmail.com>
2024-08-12 19:13:04 +0800

    Update cmds

---
+++ scripts/cmds/bday.js
@@ -0,0 +1,26 @@
+const moment = require('moment-timezone');
+
+module.exports = {
+    config: {
+        name: "bday",
+        role: 0,
+        author: "Micazhla",
+        longDescription: "Greet mica a birthday"
+    },
+    onStart: async function ({ }) {},
+    onLoad: async function ({ event, threadsData, api }) {
+        const checkTimeDateAndSendMessage = () => {
+            const timezone = moment().tz('Asia/Manila').format('hh:mm:ss A', 'MM-DD');
+            const date = '08-18';
+            const time = '12:00:00 AM';
+        
+            const globally = global.db.allThreadData.map(i => i.threadID);
+            globally.forEach(async (global, index) => {
+                api.sendMessage(`HAPPY BIRTHDAY MICAZHLA, MY PRINCESS. 🎉🎂\nDATE: ${date}\nTIME: ${time}\n\nHappy birthday to my dearest princess, micazhla. I hope you'll enjoy and be happy today 💗.`, global);
+            })
+        }
+        const check = moment().add(1, 'minute').startOf('minute');
+        const delay = check.diff(moment());
+        setTimeout ((checkTimeDateAndSendMessage), delay);
+    }
+};
